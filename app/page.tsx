c6a4611bc15d3cf675cc0cff20b4249c3d53f93c
Eduardosent <eduardogomez.sent2@gmail.com>
2026-02-27 15:33:27 -0600

    fix: login google fix 4

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
 import { Footer, Hero, Navbar } from "@/components/landing";
-import { supabase } from "@/config";
-import { useQueryClient } from "@tanstack/react-query";
-import Image from "next/image";
-import router, { useRouter } from "next/router";
-import { useEffect } from "react";
 
 export default function Home() {
   return (
