2444e5f6babb2d6f4554beef2aeb7dcbc310ea54
Eduardosent <eduardogomez.sent2@gmail.com>
2026-02-27 15:01:22 -0600

    fix: login google fix 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { Footer, Hero, Navbar } from "@/components/landing";
 import { supabase } from "@/config";
 import { useQueryClient } from "@tanstack/react-query";
@@ -8,31 +6,6 @@ import router, { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function Home() {
-      const router = useRouter();
-    const queryClient = useQueryClient();
-      useEffect(() => {
-          const exchangeCode = async () => {
-              const searchParams = new URLSearchParams(window.location.search);
-              const code = searchParams.get('code');
-              router.replace('/properties');
-  
-              if (code) {
-                  const { data, error } = await supabase.auth.exchangeCodeForSession(code);               
-                  if (error || !data.user) {
-                      return router.replace('/login?error=auth_failed');
-                  }
-                  try {
-                      router.replace('/properties');
-                  } catch (err) {
-                      router.replace('/properties');
-                  }
-              } else {
-                  router.replace('/login');
-              }
-          };
-  
-          exchangeCode();
-      }, [router, queryClient]);
   return (
     <div className="bg-[#F8FAFC]">
       <Navbar/>
